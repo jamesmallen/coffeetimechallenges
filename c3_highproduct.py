76354392840a35ca398d4658c6e7da9568236e0d
James Allen <james.m.allen@gmail.com>
2014-06-26 11:04:28 -0400

    Long brute-force solution for c3, but it works

--- c3_highproduct.py
+++ c3_highproduct.py
@@ -15,10 +15,29 @@ def permutes(s):
     
     return ret
     
-x = permutes('0123456789')
+def whatsleft(s):
+    ret = ''
+    for i in range(10):
+        if not str(i) in s:
+            ret += str(i)
+    
+    return ret
+
+ps1 = permutes('0123456789')
 # this uses a lot of memory - there are a lot of permutations!
 
+highest = 0
+multiples = (0, 0)
 
+for m1 in ps1:
+    ps2 = permutes(whatsleft(m1))
+    for m2 in ps2:
+        m1 = int(m1)
+        m2 = int(m2)
+        if m1 * m2 > highest:
+            multiples = m1, m2
+            highest = m1 * m2
+            print "%d * %d = %d" % (m1, m2, highest)
 
 
 
